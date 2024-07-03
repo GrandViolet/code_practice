cf2f4cf9243301cb2033280e85aae7d38a75e59f
Grand Violet <grandvioletofficial@gmail.com>
2024-07-03 19:14:08 -0400

    Began Minesweeper v2

---
+++ minesweeper/c/v2/minesweeper.c
@@ -0,0 +1,23 @@
+/*
+ * Play the game of Minesweeper
+ * Visualizations done using ncurses
+ * 
+ * V. Buckley
+ * Started: 07.03.2024
+ * 
+ * v2.000
+ */
+
+
+
+/******************************* Header Files *******************************/
+
+#include <ncurses.h>
+
+
+/******************************** Functions ********************************/
+
+int main() {
+
+    return 0;
+}
